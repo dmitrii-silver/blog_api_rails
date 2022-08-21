a225917d5afa5bcb1aaa01e2b57f43ac3808b235
dmitrii-silver <dmitrii.serebriakov@hotmail.com>
2022-08-21 09:47:52 +0100

    Prepare a structure for adding an avatar

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth', defaults: { format: :json }
       resources :posts, defaults: { format: :json }
       resources :comments, defaults: { format: :json }
+      resources :profiles, only: [:show]
     end
   end
 end
