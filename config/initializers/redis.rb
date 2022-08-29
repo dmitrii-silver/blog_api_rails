568e3f543e7a4c5f71564bb06f0ccc10d5110975
dmitrii-silver <dmitrii.serebriakov@hotmail.com>
2022-08-29 20:51:57 +0100

    Fixed initialize/radis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
-if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
-end
+# if ENV["REDISCLOUD_URL"]
+#     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+# end
+
+uri = URI.parse(ENV["REDISCLOUD_URL"])
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
